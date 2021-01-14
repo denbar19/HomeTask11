64d4a3128b470a0ddc5cfd38e4134fc2ce308a21
denbar <denisenko924@gmail.com>
2021-01-14 22:43:09 +0200

    right using of exception

--- src/com/denisenko/HT10_1/Main.java
+++ src/com/denisenko/HT10_1/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
         String test = "my car is dead";
         String test1 = "bread";
-        myMethod(test, test1);
+        try {
+            myMethod(test, test1);
+        }catch (IllegalArgumentException e){
+            System.out.println("Last symbols are the same: " + e.getMessage());
+        }
     }
 
     public static void myMethod(String s1, String s2) throws IllegalArgumentException {
@@ -13,9 +17,7 @@ public class Main {
         String s1Last = String.valueOf(s1.charAt(s1.length() - 1));
         String s2Last = String.valueOf(s2.charAt(s2.length() - 1));
         if (s1Last.equals(s2Last)) {
-            RuntimeException e = new IllegalArgumentException(s1Last);
-            System.out.println("last symbols are the same: " + e.getMessage());
-            throw e;
+            throw new IllegalArgumentException(s1Last);
         } else {
             System.out.println(sOut.append(s1).append(" - ").append(s2));
         }
